dece471632501518d6eeea714be9baa2c46d0f1a
Kamil Strzempowicz <konserw@gmail.com>
2013-09-28 15:54:30 +0200

    skrypt konwertujacy - umknal wczesniej :P

---
+++ res/convert.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+//my license and desc placeholder
+"""
+
+import sys
+
+sys.path.append("/home/konserw/git/Qnapi/")
+
+import aeidon
+from aeidon import *
+from aeidon import Project
+
+project = aeidon.Project()
+project.open_main("%1", "cp1250")
+project.set_framerate(aeidon.framerates.%3)
+project.save_main(aeidon.files.new(aeidon.formats.SUBRIP,"%2","utf_8"))
