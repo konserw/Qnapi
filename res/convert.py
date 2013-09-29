84a8fe10bea6a0fcc441854ed88e6fb1283928b5
Kamil Strzempowicz <konserw@gmail.com>
2013-09-29 22:06:23 +0200

    return status z convert.py

--- res/convert.py
+++ res/convert.py
@@ -2,16 +2,15 @@
 """
 copyright (c) 2013 by Kamil Strzempowicz
 """
-"""
-import sys
-sys.path.append("/home/konserw/git/Qnapi/"
-"""
 
+import sys
 import aeidon
-from aeidon import *
+"""from aeidon import *"""
 from aeidon import Project
 
 project = aeidon.Project()
 project.open_main("%1", "cp1250")
 project.set_framerate(aeidon.framerates.%3)
 project.save_main(aeidon.files.new(aeidon.formats.SUBRIP,"%2","utf_8"))
+
+sys.exit(0)
